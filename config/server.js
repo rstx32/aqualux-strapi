6f32b4e2ddc9f1eb9b0733323291a5cce42fdc84
rstx32 <rstx32@gmail.com>
2020-10-30 07:20:49 +0700

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '798ff504fe6214923f554cca5438bf95'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'f1b8eecf-098d-4066-b92d-a6d74f176f95'
+};
